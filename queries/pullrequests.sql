4b078a8e9b65d0436ce709c7323130f92438ab1a
Tamura Yukichi <tamura.yukichi@gmail.com>
2021-06-22 21:34:39 +0900

    fix query duplicate

--- queries/pullrequests.sql
+++ queries/pullrequests.sql
@@ -13,26 +13,36 @@ SELECT opened.id                 as id,
        opened.first_commit       as first_commit_sha,
        push.time_created         as first_commit_at,
        opened.repo               as repo
-FROM (SELECT id,
-             time_created,
-             JSON_VALUE(metadata, '$.pull_request.head.sha') as first_commit,
-             JSON_VALUE(metadata, '$.repository.name')       as repo
+
+FROM # opened pull request
+     (SELECT DISTINCT time_created,
+                      id,
+                      JSON_VALUE(metadata, '$.pull_request.head.sha') as first_commit,
+                      JSON_VALUE(metadata, '$.repository.name')       as repo
       FROM `four_keys.events_raw`
       WHERE event_type = 'pull_request'
         AND source = 'github'
         AND JSON_VALUE(metadata, '$.action') = 'opened') opened
+
+     # closed pull request
          LEFT JOIN
-     (SELECT id,
-             time_created,
-             JSON_QUERY(metadata, '$.pull_request.labels')         as labels
+     (SELECT DISTINCT time_created,
+                      id,
+                      JSON_QUERY(metadata, '$.pull_request.labels') as labels,
+                      JSON_VALUE(metadata, '$.repository.name')     as repo
       FROM `four_keys.events_raw`
       WHERE event_type = 'pull_request'
         AND source = 'github'
-        AND (JSON_VALUE(metadata, '$.action') = 'closed' AND
-             JSON_VALUE(metadata, '$.pull_request.merged') = 'true')) closed
-     on closed.id = opened.id
-         JOIN
-     (SELECT id,
-             JSON_VALUE(metadata, '$.head_commit.author.name') as author,
-             time_created
-      FROM `four_keys.events_raw`) push on push.id = opened.first_commit
+        AND (JSON_VALUE(metadata, '$.action') = 'closed'
+          AND JSON_VALUE(metadata, '$.pull_request.merged') = 'true')) closed
+ON closed.id = opened.id AND opened.repo = closed.repo
+
+    # first commit
+    JOIN
+    (SELECT DISTINCT time_created,
+    id,
+    JSON_VALUE(metadata, '$.head_commit.author.name') as author
+    FROM `four_keys.events_raw`
+    WHERE event_type = 'push'
+    AND source = 'github') push
+    ON push.id = opened.first_commit
